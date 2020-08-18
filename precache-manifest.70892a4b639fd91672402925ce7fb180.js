0b74a7244dfc9d653ee3f231a9c09ad29792ea73
Luka Vuković <vukovicluka227@gmail.com>
2020-08-18 19:56:53 +0200

    Updates

---
+++ precache-manifest.70892a4b639fd91672402925ce7fb180.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f7b4d91bc5ed7aa7fe08f84a6996d8",
+    "url": "/zero-molecule-cinema/index.html"
+  },
+  {
+    "revision": "3f37608e371f65ac0a59",
+    "url": "/zero-molecule-cinema/static/css/main.1dd1625b.chunk.css"
+  },
+  {
+    "revision": "cff3ddcaac897a4a0162",
+    "url": "/zero-molecule-cinema/static/js/2.7fbd6ad3.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/zero-molecule-cinema/static/js/2.7fbd6ad3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f37608e371f65ac0a59",
+    "url": "/zero-molecule-cinema/static/js/main.b770a674.chunk.js"
+  },
+  {
+    "revision": "d1f791085b28e359a957",
+    "url": "/zero-molecule-cinema/static/js/runtime-main.c2b7e157.js"
+  },
+  {
+    "revision": "8827b2644a76b83031652740cdaba45d",
+    "url": "/zero-molecule-cinema/static/media/editMovieImg.8827b264.png"
+  },
+  {
+    "revision": "79dde56323a1e72b06af9a7aedbe4d3f",
+    "url": "/zero-molecule-cinema/static/media/loginImg.79dde563.png"
+  },
+  {
+    "revision": "e4ce87c138ab887e3c21f38cb4f9b356",
+    "url": "/zero-molecule-cinema/static/media/main.e4ce87c1.png"
+  },
+  {
+    "revision": "bb3bf03da3971a270eba3bb366078689",
+    "url": "/zero-molecule-cinema/static/media/newMovieImg.bb3bf03d.png"
+  }
+]);
